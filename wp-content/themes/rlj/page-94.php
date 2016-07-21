8dab12ee9d61ebdf05ac19a7e66e549ae7209d93
Brad Sawicki <git@bradsawicki.com>
2016-07-21 20:15:16 +0200

    Brand page

---
+++ wp-content/themes/rlj/page-94.php
@@ -0,0 +1,168 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+  <section class="hero brand-hero">
+    <div class="hero-container">
+      <h1 class="hero--title">Brand guidlines and graphics:
+Richard L. Johnson Associates Inc.</h1>
+    </div>
+  </section>
+
+  <main class="main" role="main">
+
+    <section class="brand">
+      <div class="container">
+        <div class="brand-row">
+          <h2 class="section-title">Brand Guilelines</h2>
+          <div class="body-content">
+            <p>Richard L. Johnson Associates was founded in 1981 by Richard Johnson and has sense worked with numerous of clients to develop each project’s program and schematic design. In 2016 to better represent and communicate the work of RLJ we launched a new master brand. This visual brand represents the whole of the firm.</p>
+            <p>The resources on this site will help you become someone who can visually tell the story of RLJ in a cohesive, persuasive, and affective way.  With guidelines for everything from logos to fonts to color palettes, this toolkit will help you to communicate about RLJ in a way that consistently and accurately reflects what makes the our firm distinct.</p>
+            <p><a href="" class="btn">Download Brand Guide</a></p>
+          </div>
+        </div>
+        <div class="brand-row">
+          <h2 class="section-title">Image Guilelines</h2>
+          <div class="body-content">
+            <p>To maintain consistency within images all photographs used on the RLJ website and used within RLJ promotions (web and print) are to be black and white. The only exceptions are when images are used for project posts. Within project posts professionally color corrected images may be in color and digital renderings may be in color.  For print materials only digital renderings may be printed in color.</p>
+          </div>
+        </div>
+        <div class="brand-row">
+          <h2 class="section-title">Logo Guilelines</h2>
+          <div class="body-content">
+            <p>The “RLJ” logo is a company brand mark and cannot be altered in any way. It is intended to represent RLJ through it’s bold coloring, abstract representation of the letters, “RLJ” and through it’s simplicity in design. The logo accurately projects that RLJ, founded in 1981, is established, credible, and creative in our architectual solutions.</p>
+            <p>All communications, both print and electronic, published by “Richard L. Johnson Associates Inc.”, must prominently display an approved logo. There are 2 variations of the RLJ logo: horizontal and the stand alone logo mark. The individual logo mark should only be used where it will be clearly understood what the mark stands for. Since this is a new brand mark, it’s important that the audience seeing this mark knows it stands for RLJ. It may be used in communities that are currently knowledgeable such as internal use. It should not be used in broad, public circumstances (ie. highway billboard) until the mark is “seeded”, which means it is well understood by the audience. Use your best judgment regarding use of the mark.</p>
+          </div>
+        </div>
+        <div class="brand-row typeface-guidelines">
+          <h2 class="section-title">Typeface Guilelines</h2>
+          <div class="body-content">
+            <h2 class="brand-subtitle">Logo Type: Montserrat</h2>
+            <p>RLJ uses Montserrat for all headers, sub headers and logo uses. For optimal readability, this header type should have a line length of 1-5 words. Type size varies based on the platform, but for general marketing pages and documents headers should start at size 15pt, sub head 12pt, sub sub head 9pt. This type should not be used in any other context.</p>
+            <p class="download-link"><a href="#">Download Montserrat ></a></p>
+            <h2 class="brand-subtitle">Body Type: Source Sans Pro</h2>
+            <p>RLJ uses Source Sans Pro for body type and any other needed type. When in doubt choose Source Sans Pro. For optimal readability, the maximum line length should equal 50-75 words. Type size varies based on the platform, but for general marketing pages and documents body copy should be 9pt. Use proper quotes, accents, elipsis, and dashes for all copy.</p>
+            <p class="download-link"><a href="#">Download Source Sans Pro ></a></p>
+          </div>
+        </div>
+        <div class="brand-row">
+          <h2 class="section-title">Color Guilelines</h2>
+          <div class="brand-colors">
+            <div class="brand-colors--item">
+              <div class="color-swatch color-swatch__red"></div>
+              <div class="color-info">
+                <h3 class="brand-subtitle">Color Code Red</h3>
+                <p>CMYK: C15% M96% Y100% K8%</p>
+                <p>RGB: R191 G32 B000</p>
+                <p>Web: #bf2000</p>
+              </div>
+            </div>
+            <div class="brand-colors--item">
+              <div class="color-swatch color-swatch__black"></div>
+              <div class="color-info">
+                <h3 class="brand-subtitle">Color Code Black</h3>
+                <p>CMYK: C20% M0% Y0% K100%
+                <p>RGB: R000 G000 B000</p>
+                <p>Web: #000000</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="brand-row logo-downloads">
+          <h2 class="section-title">Logo Downloads</h2>
+          <div class="body-content">
+            <p>All logo downloads come in EPS, PNG, and JPG.</p>
+          </div>
+          <div class="brand-logo-list">
+            <div class="brand-logo-list--item">
+              <h3 class="brand-subtitle">Color Logo</h3>
+              <div class="brand-logo-list--details">
+                <div class="brand-logo-list--image brand-logo-list--image__color">
+                  <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo.svg">
+                </div>
+                <div class="brand-logo-list--download">
+                  <a href="" class="btn">Download</a>
+                </div>
+              </div>
+            </div>
+            <div class="brand-logo-list--item">
+              <h3 class="brand-subtitle">Black Logo</h3>
+              <div class="brand-logo-list--details">
+                <div class="brand-logo-list--image brand-logo-list--image__black">
+                  <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-black.svg">
+                </div>
+                <div class="brand-logo-list--download">
+                  <a href="" class="btn">Download</a>
+                </div>
+              </div>
+            </div>
+            <div class="brand-logo-list--item">
+              <h3 class="brand-subtitle">White Logo</h3>
+              <div class="brand-logo-list--details">
+                <div class="brand-logo-list--image brand-logo-list--image__white">
+                  <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-white.svg">
+                </div>
+                <div class="brand-logo-list--download">
+                  <a href="" class="btn">Download</a>
+                </div>
+              </div>
+            </div>
+            <div class="brand-logo-list--item">
+              <h3 class="brand-subtitle">Grey Logo</h3>
+              <div class="brand-logo-list--details">
+                <div class="brand-logo-list--image brand-logo-list--image__grey">
+                  <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-grey.svg">
+                </div>
+                <div class="brand-logo-list--download">
+                  <a href="" class="btn">Download</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="brand-logo-grid grid">
+            <div class="brand-logo-grid--item col-1-4">
+              <h3 class="brand-subtitle">Color Logo Mark</h3>
+              <div class="brand-logo-grid--image brand-logo-grid--image__color">
+                <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-mark-red.svg">
+              </div>
+              <div class="brand-logo-grid--download">
+                <a href="" class="btn">Download</a>
+              </div>
+            </div>
+            <div class="brand-logo-grid--item col-1-4">
+              <h3 class="brand-subtitle">Black Logo Mark</h3>
+              <div class="brand-logo-grid--image brand-logo-grid--image__black">
+                <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-mark-black.svg">
+              </div>
+              <div class="brand-logo-grid--download">
+                <a href="" class="btn">Download</a>
+              </div>
+            </div>
+            <div class="brand-logo-grid--item col-1-4">
+              <h3 class="brand-subtitle">White Logo Mark</h3>
+              <div class="brand-logo-grid--image brand-logo-grid--image__white">
+                <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-mark-white.svg">
+              </div>
+              <div class="brand-logo-grid--download">
+                <a href="" class="btn">Download</a>
+              </div>
+            </div>
+            <div class="brand-logo-grid--item col-1-4">
+              <h3 class="brand-subtitle">Grey Logo Mark</h3>
+              <div class="brand-logo-grid--image brand-logo-grid--image__grey">
+                <img src="<?php bloginfo('template_directory'); ?>/dist/images/logo-mark-grey.svg">
+              </div>
+              <div class="brand-logo-grid--download">
+                <a href="" class="btn">Download</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+	</main>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
